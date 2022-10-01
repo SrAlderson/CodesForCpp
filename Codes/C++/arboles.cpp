ee5ac694f27d20505474c26ff63455b662b92205
SrAlderson <srgonzalez1997@hotmail.com>
2022-10-01 02:55:33 -0500

    Arboles Binarios | Adiccion funcion eliminar

--- Codes/C++/arboles.cpp
+++ Codes/C++/arboles.cpp
@@ -12,8 +12,9 @@
 
 //Prototipo de funciones
 
-void insertar(struct nodo **);
-void buscarElemento (struct nodo **);
+void insertar(struct nodo **,int datoF);
+void buscarElemento (struct nodo **, int dato);
+void eliminarNodo (struct nodo **, int datoE);
 void preOrden (struct nodo *);
 void inOrden(struct nodo *);
 void postOrden(struct nodo *);
@@ -25,6 +26,7 @@ struct nodo {
     struct nodo *izquierda;
     int datoN;
     struct nodo *derecha;
+    struct nodo *padre; 
 
 };
 
@@ -42,7 +44,10 @@ int main(){
         scanf("%i", &opcion);
         switch (opcion){
             case 1:
-                insertar(&p);
+                int datoF;
+                printf("Por favor ingrese un dato: ");
+                scanf("%i", &datoF);
+                insertar(&p, datoF);
                 op = 1;
             break;
             case 2:
@@ -63,6 +68,21 @@ int main(){
                 printf("\n");
                 op = 1; 
             break;
+            case 5:
+                int dato;
+                printf("Por favor ingrese el dato que desea buscar: ");
+                scanf("%i", &dato);
+                buscarElemento(&p, dato);
+                op = 1; 
+            break;
+            case 6:
+                //Caso -> Eliminar
+                int datoE;
+                printf("Por favor ingrese el dato que desea eliminar: ");
+                scanf("%i", &datoE);
+                eliminarNodo(&p, datoE);
+                op = 1;
+            break;
             default:
                 op = 0; 
             break;
@@ -73,16 +93,12 @@ int main(){
 
 //Funciones
 
-void insertar(struct nodo **p){
+void insertar(struct nodo **p, int datoF){
     // Se crea una copia del arbol
     struct nodo *q; 
-
     //Para registar el dato
-    int datoF;
     int flag = 0;
-    printf("Por favor ingrese un dato: ");
-    scanf("%i", &datoF);
-
+    
     q = localizar; 
     q->datoN = datoF;
     q->izquierda = NULL;
@@ -119,9 +135,89 @@ void insertar(struct nodo **p){
         }
     }
 }
-void buscarElemento (struct nodo **){
+void buscarElemento (struct nodo **p, int dato){
+    struct nodo *aux = *p;
+    //Caso 1: Arbol esta vacio      
+    if (*p==NULL){
+        printf("Arbol esta vacio o el dato no existe\n");
+    }else{
+        if (dato == aux->datoN){
+            printf("El dato se ha encontrado.\nValor encontrado: %i \n", aux->datoN);
+        }else{
+            if (dato < aux->datoN){
+                aux = aux->izquierda;
+                *p = aux;
+                buscarElemento(p, dato);
+            }else{
+                aux = aux ->derecha;
+                *p = aux;
+                buscarElemento(p, dato);
+            }
+        }
+    }
+}
+
+void eliminarNodo(struct nodo **p, int datoE){
+    // Se crea una copia del arbol para trabajar con ella
+    struct nodo *aux = *p;
+    //Recorrido del arbol
+    //Para cuando el arbol esta vacio
+    if (*p == NULL){
+        printf("Arbol se encuentra vacio no hay elementos para eliminar. \n");
+    }else if (datoE < aux->datoN){
+        // Cuando el dato a elimnar es menor al dato del NODO
+        aux = aux->izquierda; 
+        *p=aux; 
+        printf("IZ\n");
+        eliminarNodo(p,datoE);
+    }else if(datoE > aux->datoN){
+        //Cuando el dato a eliminar es mayor al dato del NODO
+        aux = aux->derecha;
+        *p=aux; 
+        printf("DR\n");
+        eliminarNodo(p,datoE);
+    }else{
+        //Cuando el dato a eliminar es igual al dato NODO
+        
+        //Caso 1: Nodo con subarbol derecho e izquierdo
+        if (aux->derecha && aux->izquierda){
+            printf("tuki 1\n");
+            printf("dato aux: %i\n", aux->datoN);
+            //Evaluando la parte mas izquierda del sub arbol derecho
+            struct nodo *q;
+            q = *p;
+            q = q->derecha; 
+            while (q->izquierda != NULL){
+                q = q->izquierda; 
+            }
+            printf("El nodo mas izquierdo es: %i\n", q->datoN);
+            // Reemplazo del nodo mas izquierdo por el nodo a eliminar
+            aux->datoN = q->datoN;
+        }
+        //Caso 2: Nodo con subarbol derecho o izquierdo
+        if (aux->izquierda == NULL && aux->derecha!=NULL){
+            printf("tuki 2\n");
+            struct nodo *q; 
+            q = *p;
+            q = q->derecha;
+            aux = q; 
+            q->derecha = NULL;
+            q->izquierda = NULL;
+            delete q;
+        }
+    }
+
+
+
+
+//Caso 3: Nodo hoja
+
+
+
 
 }
+
+
 
 void preOrden (struct nodo *p){
     
